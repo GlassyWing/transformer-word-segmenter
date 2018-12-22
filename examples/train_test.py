ac740575b2a1c88e307ca3d85829f554e4a8bd30
manlier <1490215053@qq.com>
2018-12-22 12:07:17 +0800

    update example

--- examples/train_test.py
+++ examples/train_test.py
@@ -1,7 +1,7 @@
 from keras.callbacks import TensorBoard
 from keras.optimizers import Adam
 
-from segmenter.custom.callbacks import LRFinder, LRSchedulerPerStep, SingleModelCK
+from segmenter.custom.callbacks import LRFinder, LRSchedulerPerStep, SingleModelCK, SGDRScheduler
 from segmenter import get_or_create, save_config
 from segmenter.data_loader import DataLoader
 
@@ -10,14 +10,14 @@ if __name__ == '__main__':
     valid_file_path = "../data/2014/valid"  # 验证文件目录
     config_save_path = "../data/default-config.json"  # 模型配置路径
     weights_save_path = "../models/weights.{epoch:02d}-{val_loss:.2f}.h5"  # 模型权重保存路径
-    init_weights_path = "../models/weights.35-0.06.h5"  # 预训练模型权重文件路径
+    init_weights_path = "../models/weights.17-0.07.h5"  # 预训练模型权重文件路径
 
     src_dict_path = "../data/src_dict.json"  # 源字典路径
     tgt_dict_path = "../data/tgt_dict.json"  # 目标字典路径
     batch_size = 32
     epochs = 128
-    num_gpu = 4
-    max_seq_len = 512
+    num_gpu = 1
+    max_seq_len = 256
 
     # import os
     #
@@ -40,14 +40,17 @@ if __name__ == '__main__':
         'src_vocab_size': data_loader.src_vocab_size,
         'tgt_vocab_size': data_loader.tgt_vocab_size,
         'max_seq_len': max_seq_len,
-        'max_depth': 8,
+        'max_depth': 2,
         'model_dim': 320,
-        'residual_dropout': 0.0,
-        'attention_dropout': 0.0,
+        'residual_dropout': 0.1,
+        'attention_dropout': 0.2,
+        'l2_reg_penalty': 1e-6,
+        'confidence_penalty_weight': 0.1,
         'compression_window_size': None,
         'use_masking': True,
         'num_heads': 8,
-        'use_crf': True
+        'use_crf': False,
+        'label_smooth': True
     }
 
     # K.set_session(get_session(0.9))
@@ -78,10 +81,10 @@ if __name__ == '__main__':
     # Use LRFinder to find effective learning rate
     lr_finder = LRFinder(1e-6, 1e-2, steps_per_epoch, epochs=1)  # => (2e-4, 3e-4)
     lr_scheduler = LRSchedulerPerStep(segmenter.model_dim)
-    # lr_scheduler = SGDRScheduler(min_lr=5e-4, max_lr=1e-3, steps_per_epoch=steps_per_epoch,
-    #                              cycle_length=10,
-    #                              lr_decay=0.87,
-    #                              mult_factor=1.2)
+    # lr_scheduler = SGDRScheduler(min_lr=2e-5, max_lr=2e-4, steps_per_epoch=steps_per_epoch,
+    #                              cycle_length=15,
+    #                              lr_decay=0.96,
+    #                              mult_factor=1.3)
 
     segmenter.parallel_model.fit_generator(data_loader.generator(train_file_path),
                                            epochs=epochs,
