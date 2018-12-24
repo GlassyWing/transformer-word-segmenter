0bab54dedd5066c0420c1815641c806afc52dc00
GlassyWing <1490215053@qq.com>
2018-12-24 15:14:14 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setup(name='transformer-word-segmenter',
 
       zip_safe=False,
 
-      install_requires=['pandas', 'matplotlib', 'keras'],
+      install_requires=['pandas', 'matplotlib', 'keras', 'keras-transformer'],
 
       )
