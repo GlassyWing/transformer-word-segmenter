8d8040afdbf869014c10f9671a908d73845e5de2
GlassyWing <1490215053@qq.com>
2018-12-26 10:03:44 +0800

    update examples

--- examples/make_dicts_example.py
+++ examples/make_dicts_example.py
@@ -1,7 +1,7 @@
 from tf_segmenter.utils import make_dictionaries
 
 if __name__ == '__main__':
-    make_dictionaries("../data/2014",
+    make_dictionaries("../data/2014_processed",
                           src_dict_path="../data/src_dict.json",
                           tgt_dict_path="../data/tgt_dict.json",
                           filters="\t\n",
